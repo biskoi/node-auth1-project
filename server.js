53e67eed043fcea2105a111b11c42a3a6f84e686
biskoi <biskoi@daydream.cafe>
2020-04-20 22:47:20 -0600

    project mvp

--- server.js
+++ server.js
@@ -2,7 +2,24 @@ const express = require('express');
 const db = require('./user-scheme');
 const server = express();
 const bcrypt = require('bcryptjs')
+const auth = require('./authenticator');
+const session = require('express-session')
+
+const sessionConfig = {
+   name: 'cookieName',
+   secret: process.env.SECRET || 'someSecretString',
+   resave: false,
+   saveUninitialized: process.env.SEND_COOKIES || true,
+   cookie: {
+      maxAge: 1000 * 60 * 5, // good for 5 min in ms
+      secure: process.env.USE_SECURE_COOKIES || false, // can be false over https only, set true in production. never ever send cookies over http
+      httpOnly: true, // true = JS on client can NOT access the cookies
+   }
+
+}
+
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.get('/', (req, res) => {
    res.status(200).json({
@@ -10,10 +27,12 @@ server.get('/', (req, res) => {
    })
 });
 
-server.get('/users', (req, res) => {
+server.get('/users', auth, (req, res) => {
    db.find()
       .then(rep => {
-         res.status(200).json(rep)
+         res.status(200).json({
+            data: rep
+         })
       })
 });
 
@@ -45,8 +64,10 @@ server.post('/login', (req, res) => {
       .then(user => {
 
          if (user && bcrypt.compareSync(req.body.password, user[0].password)) {
+            req.session.login = true;
+            req.session.userId = user[0].id;
             res.status(200).json({
-               message: `Login success`
+               message: `Login success`,
             });
          } else {
             res.status(400).json({
